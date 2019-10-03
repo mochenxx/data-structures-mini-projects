c08d5318c517a7a6fbe7079c23b20f06e2c5c2db
Mo Chen <46802314+mochenxx@users.noreply.github.com>
2019-10-03 17:20:13 -0400

    Update header files

--- Stack/Stack-using-array/stackHelper.h
+++ Stack/Stack-using-array/stackHelper.h
@@ -13,7 +13,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-// Data structure for stack
+/* Data structure for stack */
 struct Stack
 {
 	unsigned int capacity;
@@ -25,18 +25,18 @@ struct Stack
  * This function initializes a stack with a given capacity
  * and returns the stack pointer
  *
- * param[in] capacity - maximum capacity of the stack
+ * @param[in] capacity - maximum capacity of the stack
  *
- * returns - new stack pointer
+ * @returns - new stack pointer
  */
 struct Stack* createStack(unsigned int capacity);
 
 /*
  * This function returns the size of the stack
  *
- * param[in] stack - pointer to a given stack
+ * @param[in] stack - pointer to a given stack
  *
- * returns - size of the stack
+ * @returns - size of the stack
  */
 int size(struct Stack* stack);
 
@@ -44,9 +44,9 @@ int size(struct Stack* stack);
  * This function checks if the stack is empty or not
  * and returns the result
  *
- * param[in] stack - pointer to a given stack
+ * @param[in] stack - pointer to a given stack
  *
- * returns - 1 if stack if empty, else 0.
+ * @returns - 1 if stack if empty, else 0.
  */
 int isEmpty(struct Stack* stack);
 
@@ -54,19 +54,19 @@ int isEmpty(struct Stack* stack);
  * This function checks if the stack is full or not
  * and returns the result
  *
- * param[in] stack - pointer to a given stack
+ * @param[in] stack - pointer to a given stack
  *
- * returns - 1 if stack if full, else 0.
+ * @returns - 1 if stack if full, else 0.
  */
 int isFull(struct Stack* stack);
 
 /*
  * This function adds an item to the stack
  *
- * param[in] stack - pointer to a given stack
- * param[in] item - item to be added
+ * @param[in] stack - pointer to a given stack
+ * @param[in] item - item to be added
  *
- * returns - none
+ * @returns - none
  */
 void push(struct Stack* stack, int item);
 
@@ -74,10 +74,10 @@ void push(struct Stack* stack, int item);
  * This function gets the top from the stack without removing it, 
  * and pass the popped item value as an argument
  *
- * param[in] stack - pointer to a given stack
- * param[in] pop_val - pointer to popped item value
+ * @param[in] stack - pointer to a given stack
+ * @param[in] pop_val - pointer to popped item value
  *
- * returns - 0 if there is no error, else -1.
+ * @returns - 0 if there is no error, else -1.
  */
 int peek(struct Stack* stack, int* top_val);
 
@@ -85,46 +85,46 @@ int peek(struct Stack* stack, int* top_val);
  * This function removes an item from stack, 
  * and pass the popped item value as an argument
  *
- * param[in] stack - pointer to a given stack
- * param[in] pop_val - pointer to popped item value
+ * @param[in] stack - pointer to a given stack
+ * @param[in] pop_val - pointer to popped item value
  *
- * returns - 0 if there is no error, else -1.
+ * @returns - 0 if there is no error, else -1.
  */
 int pop(struct Stack* stack, int* pop_val);
 
 /*
  * This function display a stack in forward order
  *
- * param[in] stack - pointer to a given stack
+ * @param[in] stack - pointer to a given stack
  *
- * returns - none
+ * @returns - none
  */
 void displayStack(struct Stack* stack);
 
 /*
  * This function display a stack in backward order
  *
- * param[in] stack - pointer to a given stack
+ * @param[in] stack - pointer to a given stack
  *
- * returns - none
+ * @returns - none
  */
 void printReversedStack(struct Stack* stack);
 
 /*
  * This function reverses a stack
  *
- * param[in] stack - pointer to a given stack
+ * @param[in] stack - pointer to a given stack
  *
- * returns - none
+ * @returns - none
  */
 void reverse(struct Stack* stack);
 
 /*
  * This function free the memory allocated to a given stack
  *
- * param[in] stack - pointer to a given stack
+ * @param[in] stack - pointer to a given stack
  *
- * returns - none
+ * @returns - none
  */
 void freeStack(struct Stack* stack);
 
